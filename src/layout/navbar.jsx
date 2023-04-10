c1dfa2b237302924cd9bb30f8c090b81d2672f15
bryan98jacome <bryan98jacome@hotmail.com>
2023-04-09 20:07:35 -0500

    Configurar router final

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -13,14 +13,14 @@ export default function Navigation() {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav className="me-auto">
-                            <Nav.Link href="/">Inicio</Nav.Link>
+                            <Nav.Link href="../">Inicio</Nav.Link>
                             <NavDropdown title="Añadir" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/addproduction">Producción</NavDropdown.Item>
-                                <NavDropdown.Item href="/">Pedido</NavDropdown.Item>
+                                <NavDropdown.Item href="../addproduction">Producción</NavDropdown.Item>
+                                <NavDropdown.Item href="../">Pedido</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="/">Reingreso</NavDropdown.Item>
+                                <NavDropdown.Item href="../">Reingreso</NavDropdown.Item>
                             </NavDropdown>
-                            <Nav.Link href="/addproduct">Add Producto</Nav.Link>
+                            <Nav.Link href="../addproduct">Add Producto</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
